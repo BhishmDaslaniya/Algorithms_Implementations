3c5b83b0f7d454189176e86a7e152b27e1bb2ea9
Bhishm Daslaniya <bhishm1999@gmail.com>
2019-09-23 13:03:36 +0530

    BNMCOEF Complete

--- Design And Analysis of Algorithm/Binomial_Coefficient.cpp
+++ Design And Analysis of Algorithm/Binomial_Coefficient.cpp
@@ -2,13 +2,34 @@
 using namespace std;
 typedef long long ll;
 
-void BNMCOEF(ll n, ll k){
+ll BNMCOEF(ll n,ll k){
+    ll C[n+1][k+1] ;
+    memset(C,0,sizeof(C));
+    for(ll i = 0 ; i <= n ; i++){
+        for(ll j = 0 ; j <= min(i,k);j++){
+            if(j==0 || j==i){
+                C[i][j] = 1;
+            }else{
+                C[i][j] = C[i-1][j-1]+C[i-1][j];
+            }
+        }
+    }
+    
+    cout<<" ---------------------------------------"<<endl;
 
+    for(ll i = 0 ; i <= n ; i++){
+        for(ll j = 0; j <= k ; j++ ){
+            cout<<C[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return C[n][k];
 }
 
 int main(){
     ll n,k;
+    cout<<"Enter n and k for compute nCk : ";
     cin>>n>>k;
-    BNMCOEF(n,k);
+    cout<<"value of "<<n<<"C"<<k<<" : "<<BNMCOEF(n,k)<<endl;
     return 0;
 }
